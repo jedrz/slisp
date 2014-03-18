a8a6ca10dda94e8d6dbba5b9a93cb4bcad11a286
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2014-03-18 23:17:19 +0100

    Init

---
+++ src/main/java/com/jedrzejewski/slisp/App.java
@@ -0,0 +1,13 @@
+package com.jedrzejewski.slisp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
