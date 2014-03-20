b772a53e297cb6af3eaa11e6b96c6ef444636759
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2014-03-20 17:10:35 +0100

    More complicated test

--- src/test/com/jedrzejewski/slisp/lexer/LexerTest.java
+++ src/test/com/jedrzejewski/slisp/lexer/LexerTest.java
@@ -12,15 +12,25 @@ public class LexerTest {
 
     @Test
     public void testNextToken() {
-        Lexer lexer = new Lexer("(zero? 0)");
+        Lexer lexer = new Lexer("(fn (zero? 0) (+ 2 3))");
         List<Token> tokens = new LinkedList<Token>();
         Token token;
         while ((token = lexer.getNextToken()) != null) {
             tokens.add(token);
         }
         List<Token> expected = Arrays.asList(
-                new OpenBracket(), new Symbol("zero?"),
-                new Number(0), new CloseBracket());
+                new OpenBracket(),
+                new Symbol("fn"),
+                new OpenBracket(),
+                new Symbol("zero?"),
+                new Number(0),
+                new CloseBracket(),
+                new OpenBracket(),
+                new Symbol("+"),
+                new Number(2),
+                new Number(3),
+                new CloseBracket(),
+                new CloseBracket());
         org.junit.Assert.assertEquals(tokens, expected);
     }
 }
