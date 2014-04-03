16a4c4a29180c738b9e66fdd66681f22b6066e4e
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2014-04-03 12:46:46 +0200

    doc: simplify fn form

--- doc/impl/FnForm.java
+++ doc/impl/FnForm.java
@@ -4,21 +4,12 @@ public class FnForm implements SpecialForm {
     public LispObject call(List<LispObject> args, Interpreter.Evaluator evaluator) {
         // Obsługa parametrów.
         List<Sym> argList = new LinkedList<>();
-        if (args.get(0) instanceof Vec) {
-            Vec argVec = (Vec) args.get(0);
-            for (LispObject arg : argVec) {
-                if (arg instanceof Sym) {
-                    argList.add((Sym) arg);
-                } else {
-                    // Nazwy argumentów powinny być symbolami.
-                }
-            }
-        } else {
-            // Pierwszym argumentem powinien być wektor.
+        Vec argVec = (Vec) args.get(0);
+        for (LispObject arg : argVec) {
+            argList.add((Sym) arg);
         }
-
-        List<LispObject> body = args.subList(1, args.size());
         // Ciało funkcji.
+        List<LispObject> body = args.subList(1, args.size());
         return new Function(argList, body, evaluator.getScope());
     }
 }
