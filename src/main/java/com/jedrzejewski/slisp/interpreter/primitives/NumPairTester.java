3a27b875c7a7076bc4097990bd0f081f994e4d50
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2014-04-30 18:14:49 +0200

    Check no of args in comp primitives

--- src/main/java/com/jedrzejewski/slisp/interpreter/primitives/NumPairTester.java
+++ src/main/java/com/jedrzejewski/slisp/interpreter/primitives/NumPairTester.java
@@ -1,12 +1,18 @@
 package com.jedrzejewski.slisp.interpreter.primitives;
 
+import com.jedrzejewski.slisp.interpreter.exceptions.InterpreterException;
+import com.jedrzejewski.slisp.interpreter.exceptions.WrongNumberOfArgsException;
 import com.jedrzejewski.slisp.lispobjects.LispObject;
 import com.jedrzejewski.slisp.lispobjects.Num;
 import java.util.List;
 
 public abstract class NumPairTester extends Primitive {
 
-    public boolean testSubsequentPairs(List<LispObject> args, DoublePairPredicate predicate) {
+    public boolean testSubsequentPairs(List<LispObject> args,
+                                       DoublePairPredicate predicate)
+            throws InterpreterException {
+        validate(args);
+
         double prevValue = ((Num) args.get(0)).getValue();
         for (LispObject arg : args.subList(1, args.size())) {
             double curValue = ((Num) arg).getValue();
@@ -17,6 +23,13 @@ public abstract class NumPairTester extends Primitive {
             }
         }
         return true;
+    }
+
+    public void validate(List<LispObject> args) throws InterpreterException {
+        if (args.size() <= 1) {
+            throw WrongNumberOfArgsException.atLeast(2).is(args.size());
+        }
+        // TODO: check types.
     }
 
     public interface DoublePairPredicate {
